9f321bd6e1cb181a41808ea1b442da17b42e1c84
dev <jasonzampascy@proton.me>
2024-12-01 22:00:15 +0000

    Added symbols

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import json
 
 app = Flask(__name__)
 
-with open("currencies.json") as f:
-    currencies = json.load(f)
-
 @app.route('/', methods=['GET', 'POST'])
 def currency_converter():
     result = None
+
+    with open("currencies.json") as f:
+        currencies = json.load(f)
+    
     if request.method == 'POST':
         from_currency = request.form['from_currency']
         to_currency = request.form['to_currency']
@@ -17,11 +18,17 @@ def currency_converter():
 
         try:
             conversion_result = get_exchange_rate(from_currency.lower(), to_currency.lower(), amount)
-            result = f"{amount} {from_currency.upper()} = {conversion_result} {to_currency.upper()}"
+            
+            from_name = currencies.get(from_currency.upper(), {}).get("name", "")
+            from_symbol = currencies.get(from_currency.upper(), {}).get("symbol", "")
+            to_name = currencies.get(to_currency.upper(), {}).get("name", "")
+            to_symbol = currencies.get(to_currency.upper(), {}).get("symbol", "")
+            
+            result = f"{from_symbol}{amount} = {to_symbol}{conversion_result}"
         except KeyError as e:
             print(f"KeyError: {e}")
             result = "Invalid currency code selected."
-
+    
     return render_template('index.html', currencies=currencies, result=result)
 
 if __name__ == '__main__':
